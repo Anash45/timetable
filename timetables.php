e885b8997190b9b6a0b1e2925e9396999c6719a1
Syed Anas Bukhari <anas14529@gmail.com>
2024-04-20 20:45:29 +0500

    update 6

--- timetables.php
+++ timetables.php
@@ -123,8 +123,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && (isAdmin() || isLecturer())) {
                         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">Timetables</h1>
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#addTimetableModal"> Add Timetable </button>
+                        <?php
+                        if(isAdmin() && isLecturer()){
+                            echo '<button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#addTimetableModal"> Add Timetable </button>';
+                        }
+                        ?>
                     </div>
                     <?php echo $info; ?>
                     <div class="card shadow">
